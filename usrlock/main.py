dea7278698513a8fe98d6450dd9080fc4455c632
mashed-potatoes <git@michiru.sh>
2021-03-24 15:58:22 +0500

    hotfix

--- usrlock/main.py
+++ usrlock/main.py
@@ -84,5 +84,6 @@ def main():
     if not args.skip_bootloader:
         with open(args.manifest) as json_file:
             data = json.load(json_file)
+            data["name"] = args.bootloader
         flash_images(data)
     write_nvme(args.key)
